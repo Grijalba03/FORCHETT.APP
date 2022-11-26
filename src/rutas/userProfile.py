8194d5b9cec9b7b338ecf62acdc47769e96820e5
moniasuka <moniasuka@hotmail.com>
2022-11-26 01:20:31 +0000

    actualizando init de rutas para user

--- src/rutas/userProfile.py
+++ src/rutas/userProfile.py
@@ -1,7 +1,7 @@
 import os
 from ..main import request, jsonify, app, bcrypt
 from ..db import db
-from ..modelos import Categories 
+from ..modelos import Categories, User
 from flask import Flask, url_for
 from datetime import datetime
 import json 
