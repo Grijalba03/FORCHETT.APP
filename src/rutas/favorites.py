110a2f490888cd5bd58cfc29621cf31896888d22
Grijalba03 <arieswolve26@gmail.com>
2022-12-14 19:25:01 -0600

    Favorites READY

--- src/rutas/favorites.py
+++ src/rutas/favorites.py
@@ -12,25 +12,26 @@ from ..utils import APIException
 def get_favorites(user_id):
     if user_id==0:
         raise APIException("ID can't be 0", status_code=400)  
-    favorites = Favorites.query.filter_by(user_id = user_id).first()
+    favorites = Favorites.query.filter_by(user_id = user_id).all()
     print("these are your favorites",favorites) 
     if favorites == None:
         raise APIException("Favorites not found", status_code=400) 
-    favorites = favorites.serialize()
-    return jsonify(favorites.serialize()), 200
+    # favorites = favorites.serialize()
+    favorites = list(map( lambda fav: fav.serialize(), favorites)) 
+    return jsonify(favorites), 200
     
                 
 @app.route('/user/favorites/<int:user_id>', methods=['POST'])
-def adding_favorites():
+def adding_favorites(user_id):
     body = request.get_json()
     if body is None:
-        raise APIException("Body está vacío" , status_code=400)
-    if body['name'] is None or body['name']=="":
-        raise APIException("name es inválido" , status_code=400)
+        raise APIException("id está vacío" , status_code=400)
+    # if body['id'] is None or body['id']=="":
+    #     raise APIException("id es inválido" , status_code=400)
 
-    new_favorite = Favorites(recipe_title=body['recipe_title'], recipe_Id=body['recipe_Id'], username=body['username'], image=body['image'])
-    favorites = Favorites.query.all()
-    favorites = list(map( lambda item: item.serialize(), favorites))        
+    new_favorite = Favorites(user_id=body['user_id'], recipe_id=body['recipe_id'])
+    # favorites = Favorites.query.filter_by(user_id)
+    # favorites = list(map( lambda item: item.serialize(), favorites))        
     print(new_favorite)
     db.session.add(new_favorite) 
     db.session.commit()
