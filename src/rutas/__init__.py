789206a7173fe10d17da199f1f0b18d1497b7341
nunnoit <devit@tuta.io>
2022-12-15 22:01:04 +0000

    search

--- src/rutas/__init__.py
+++ src/rutas/__init__.py
@@ -1,4 +1,4 @@
-from .recipes import get_recipes, get_recipe_by_id
+from .recipes import get_recipes, get_recipe_by_id, search_recipe
 from .user import signup, user_login, user_logout, user_vip, get_user_by_id, delete_user_by_id, get_users, hello_protected, allUsers, update_user_account, update_user_account_password
 # from .people import create_new_person, put_people_by_id, busqueda_people, get_people, get_people_by_id, delete_character_by_id
 # from .planet import get_planets, get_planet_by_id, create_new_planet, delete_planet_by_id
